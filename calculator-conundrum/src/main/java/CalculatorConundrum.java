01f8ca164bf36bd250ec0c6fa23bbdaf6668967b
blur9 <nikohightower@gmail.com>
2026-09-08 17:56:41 +0300

    Mentor feedback: Run the exception statements first (in if) Format was repeating 3 times, wrote it once instead

--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,20 +1,22 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        }
+        if (operation.isEmpty()) {
+            throw new IllegalArgumentException("Operation cannot be empty");
+        }
         try {
-            return switch (operation) {
-                case "+" -> format(operand1, operand2, "+", operand1 + operand2);
-                case "*" -> format(operand1, operand2, "*", operand1 * operand2);
-                case "/" -> format(operand1, operand2, "/", operand1 / operand2);
-                case null -> throw new IllegalArgumentException("Operation cannot be null");
-                case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+            int result = switch (operation) {
+                case "+" -> operand1 + operand2;
+                case "*" -> operand1 * operand2;
+                case "/" -> operand1 / operand2;
                 default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
             };
-        } catch (ArithmeticException e) {
-            throw new IllegalOperationException("Division by zero is not allowed", e);
-        }
-        }
-
-        private String format(int operand1, int operand2, String operation, int result) {
-            return operand1 + " " + operation + " " + operand2 + " = " + result;
+            return "%d %s %d = %d"
+                    .formatted(operand1, operation, operand2, result);
+        } catch (ArithmeticException exception) {
+            throw new IllegalOperationException("Division by zero is not allowed", exception);
         }
     }
+}
